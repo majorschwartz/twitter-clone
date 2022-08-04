def05484bffc40a8fbeb2da18fb1641069695714
qubitflip <107882188+qubitflip@users.noreply.github.com>
2022-08-04 18:51:12 -0500

    Sidebar addition

---
+++ src/Feed.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Feed() {
+  return (
+    <div>
+      {/* Header */}
+      <h2>Home Page</h2>
+
+      {/* TweetBox */}
+
+      {/* Posts */}
+    </div>
+  );
+}
+
+export default Feed;
