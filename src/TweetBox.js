17995e7e430fd9ffd5d8384906b0fd8d7281ccd9
qubitflip <107882188+qubitflip@users.noreply.github.com>
2022-08-05 12:19:21 -0500

    Tweet Box changes

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./TweetBox.css";
+import { Avatar, Button } from "@material-ui/core";
 
 function TweetBox() {
   return (
-    <div className="tweetbox">
-      <h2>Tweet Box</h2>
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar src="https://cdn.iconscout.com/icon/free/png-256/profile-2337176-1982537.png" />
+          <input placeholder="What's happening?" type="text"></input>
+          {/* <input placeholder="Enter image URL" type="text"></input> */}
+        </div>
+        <Button className="tweetBox__tweetButton">Tweet</Button>
+      </form>
     </div>
   );
 }
