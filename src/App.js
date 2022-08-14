11fe31031aea1d401c1b7ee9bb598d29b30936fc
qubitflip <107882188+qubitflip@users.noreply.github.com>
2022-08-14 10:31:09 -0500

    Removed unnecessary comments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,3 @@
-/*
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBu2Y2EaY-21mEDIr6BSZPCeb7qnNjoNSs",
-  authDomain: "twitter-clone-a1237.firebaseapp.com",
-  projectId: "twitter-clone-a1237",
-  storageBucket: "twitter-clone-a1237.appspot.com",
-  messagingSenderId: "369581583957",
-  appId: "1:369581583957:web:ac444218d5efff18b16edc"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-*/
-
 import React from "react";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
